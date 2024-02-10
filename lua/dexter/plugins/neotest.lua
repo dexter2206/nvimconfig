821ce6e365e084b8a088b7b6122c41d7c4a98897
Konrad Jałowiecki <dexter2206@gmail.com>
2024-02-10 17:30:21 +0100

    Add neotest

---
+++ lua/dexter/plugins/neotest.lua
@@ -0,0 +1,46 @@
+return { 
+  "nvim-neotest/neotest",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "antoinemadec/FixCursorHold.nvim",
+    "nvim-treesitter/nvim-treesitter"
+  },
+  config = function()
+      local neotest = require("neotest")
+      neotest.setup({
+        highlights = {
+            dir = "Directory",
+            passed = "String",
+            adapter_name = "String",
+            failed = "@variable.builtin",
+            file = "@constructor",
+            test = "@variable"
+        },
+        adapters = {
+            require("neotest-python")({
+                runner = "pytest",
+                args = {"-vv"},
+                pytest_discover_instances = true,
+                python = function()
+                    return require("venv-selector").get_active_path()
+                end
+            })
+        },
+        summary = {
+            open = "botright vsplit | vertical resize 100",
+            mappings = {
+                run = "r",
+                mark = "<TAB>",
+                run_marked = "R",
+                stop = "k"
+            }
+        }
+    })
+    vim.keymap.set(
+      "n",
+      "<leader>ts",
+      neotest.summary.toggle
+    )
+    vim.api.nvim_set_hl(0, "NeotestPassed", { fg = vim.api.nvim_get_hl(0, { name = "String" }).fg })
+end
+}
